97e3bc6163250c969b13b19e114ef08c111d8333
javier00x <javierf7900@gmail.com>
2017-02-17 13:59:28 -0500

    ensure valid pay rate done

--- CS_Chapter3/EnsureValidPayRate/EnsureValidPayRate.cs
+++ CS_Chapter3/EnsureValidPayRate/EnsureValidPayRate.cs
@@ -14,17 +14,27 @@ namespace EnsureValidPayRate
 
             if (rate > 49.99)
             {
-                Console.WriteLine("ERROR! please re-enter your desired pay rate");
+                Console.WriteLine("ERROR! please re-enter your desired pay rate. ");
                 Console.Write("Enter your pay rate here: ");
                 rate = Convert.ToDouble(Console.ReadLine());
 
                 if (rate > 49.99)
-
+                {
+                    Console.WriteLine("your desired pay rate has been denied! sorry");
+                }  
             }
 
             if (rate < 5.65)
             {
-                Console.WriteLine("ERROR!");
+                Console.WriteLine("ERROR! please re-enter your desired pay rate");
+                Console.Write("Enter your pay rate here: ");
+                rate = Convert.ToDouble(Console.ReadLine());
+
+                if (rate < 5.65)
+                {
+                    Console.WriteLine("Your desired pay rate has been denied, I'm sorry!");
+                }
+
             }
 
             if (rate < 5.0 || rate > 50.0)
--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -9,6 +9,43 @@ namespace EnsureValidPayRate
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a pay rate: ");
+            double rate = Convert.ToDouble(Console.ReadLine());
+
+            if (rate > 49.99)
+            {
+                Console.WriteLine("ERROR! please re-enter your desired pay rate. ");
+                Console.Write("Enter your pay rate here: ");
+                rate = Convert.ToDouble(Console.ReadLine());
+
+                if (rate > 49.99)
+                {
+                    Console.WriteLine("your desired pay rate has been denied! sorry");
+                }  
+            }
+
+            if (rate < 5.65)
+            {
+                Console.WriteLine("ERROR! please re-enter your desired pay rate");
+                Console.Write("Enter your pay rate here: ");
+                rate = Convert.ToDouble(Console.ReadLine());
+
+                if (rate < 5.65)
+                {
+                    Console.WriteLine("Your desired pay rate has been denied, I'm sorry!");
+                }
+
+            }
+
+            if (rate < 5.0 || rate > 50.0)
+            {
+                // Out of range
+            }
+
+            if (rate >= 5.0 && rate <= 50.0)
+            {
+                // In range
+            }
         }
     }
 }
